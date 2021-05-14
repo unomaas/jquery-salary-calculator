6b219712eddee09ce81337e92da7369f47eb8bef
Ryan Maas <rdmjobs@live.com>
2021-05-14 15:38:46 -0500

    Added totalSalary vs monthlyCosts functionality.

--- assignment/client.js
+++ assignment/client.js
@@ -13,18 +13,21 @@ $(document).ready(function() {
 
   //#region ⬇ Setting default monthly costs: 
   let monthlyCosts = 0;
+  let totalSalary = 0;
   //#endregion ⬆ Default monthly costs above. 
 
   //#region ⬇ Submit button actions below:
   $('#submitButton').on('click', clickedSubmit)
   function clickedSubmit() {
     console.log('Test: In clickedSubmit.'); 
+
     // ⬇ Declaring variables stored with input values: 
     let firstNameInput = $('#firstNameInput').val();
     let lastNameInput = $('#lastNameInput').val();
     let idInput = $('#idInput').val();
     let titleInput = $('#titleInput').val();
     let salaryInput = $('#salaryInput').val();
+
     // ⬇ Putting those input variables on the DOM:
     $('#tableBody').append(`
       <tr>
@@ -36,16 +39,22 @@ $(document).ready(function() {
         <td class="deleteCell"><button class="deleteEmployeeButton">Delete</button></td>
       </tr>
     `); // End tableBody.append. 
+
     // ⬇ Emptying out the input fields after click.  
     $('input').val('');
-    // ⬇ Updating monthly costs after click. 
-    monthlyCosts += Number(salaryInput); // Not sure how this will function on number vs string. 
+
+    // ⬇ Updating costs after click. 
+    totalSalary += Number(salaryInput); 
+    monthlyCosts = totalSalary / 12;
     $('#monthlyOutput').text(monthlyCosts);
-    console.log('Test: Monthly Costs are currently: ', monthlyCosts);
-    // ⬇ If over $20k, add red background. 
+    console.log('Test: Total Salary is currently: ', totalSalary);
+    console.log('Test: Monthly Costs is currently: ', monthlyCosts);
+
+    // ⬇ If monthlyCosts are over budget, add red background. 
     if (monthlyCosts > 20000) {
       $('#totalMonthlyCost').addClass('overBudget');
-    } // End if overBudget. 
+    } // End if over budget. 
+
   } // End clickedSubmit function. 
   //#endregion ⬆ Submit button actions above. 
 
@@ -53,7 +62,7 @@ $(document).ready(function() {
   $('#tableBody').on('click', '.deleteEmployeeButton', clickedDelete);
   function clickedDelete() {
     console.log('Test: In clickedDelete.'); 
-    $(this).parent().parent().remove(); // Button.Cell.Row.Remove.
+    $(this).parent().parent().remove(); // Syntax: Button.Cell.Row.Remove.
     // ⬇ This is where the Stretch goal code would go, I think. 
 
   } // End clickedDelete function. 
@@ -63,40 +72,16 @@ $(document).ready(function() {
 
 });
 
+// My To Do:
+//    Figure out how to convert the Monthly Cost output to dollar amounts with commas.
+//    Figure out the Stretch #2 Goal. 
+//    Divide annual salary by 12 and use that for the monthly costs.  
+
 // Base Mode: 
   // - Step #1.) The application should have an input form that collects _employee first name, last name, ID number, job title, annual salary_. (DONE)
   // - Step #2.) A 'Submit' button should collect the form information, store the information to calculate monthly costs, append information to the DOM and clear the input fields. (DONE)
-  // - Step #3.) Using the stored information, calculate monthly costs and append this to the to DOM. If the total monthly cost exceeds $20,000, add a red background color to the total monthly cost.
-  // - Step #4.) Create a delete button that removes an employee from the DOM. For Base mode, it does **not** need to remove that Employee's salary from the reported total.
+  // - Step #3.) Using the stored information, calculate monthly costs and append this to the to DOM. If the total monthly cost exceeds $20,000, add a red background color to the total monthly cost. (DONE)
+  // - Step #4.) Create a delete button that removes an employee from the DOM. For Base mode, it does **not** need to remove that Employee's salary from the reported total. (DONE)
 // Stretch Mode: 
   // - Stretch #1.) Add styling or extra functionality that fits with the theme of this assignment.
   // - STretch #2.) Once the employee is deleted, update the total spend on salaries account for this employee's removal. This will require that the logic knows which element was removed. You will need to use `.text()` as a getter or look into jQuery's `.data()` function. This is tricky! 
-
-  //   $('#tableBody').append(`
-  //    <tr>
-  //      <td>${pet.name}</td><td>${pet.type}</td><td>${pet.age}</td>
-  //    </tr>
-  //  `);} // End #tableBody.append. 
-
-  // $('#addNewPetButton').on('click', addsNewPet);
-  // function addsNewPet() {
-  //   let petNameInput = $('#addPetNameInput').val();
-  //   let petTypeInput = $('#addPetTypeInput').val();
-  //   let petAgeInput = $('#addPetAgeInput').val();
-  //   $('#tableBody').append(`
-  //     <tr>
-  //       <td>${petNameInput}</td><td>${petTypeInput}</td><td>${petAgeInput}</td>
-  //     </tr>
-  //   `);
-  //   $('input').val('');
-  // } // End addsNewPet function. 
-
-  // //#region ⬇ Red delete boxes actions below: 
-  // $("body").on("click", ".redBoxes", deletesRedBox); 
-  // function deletesRedBox() {
-  //   $(this).remove();
-  //   $("#redCounterOutput").empty();
-  //   redCounterOutput -= 1;
-  //   $("#redCounterOutput").append(redCounterOutput);
-  // }
-  // //#endregion ⬆ Red delete boxes actions below: 
